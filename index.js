966bf843c432f3fe4e40b099e334c1013bd51be6
hakeem <hakeem@codeap.com>
2024-06-19 22:54:50 +0530

    changes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
@@ -19,7 +20,7 @@ const likesRouter = require("./routes/Likes");
 app.use("/likes", likesRouter);
 
 db.sequelize.sync().then(() => {
-  app.listen(3001, () => {
+  app.listen(process.env.PORT || 3001, () => {
     console.log("Server running on port 3001");
   });
 });
